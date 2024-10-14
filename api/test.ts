5d3c36c43fb65e1cf98324a6b5feec680820ea0f
jammy312 <james.brutus@polymtl.ca>
2024-10-13 21:09:24 -0400

    Update test.ts

--- api/test.ts
+++ api/test.ts
@@ -1,8 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { NextRequest } from 'next/server'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
+export async function POST(req: NextRequest) {
+  return Response.json({
     message: `Hello ${name}!`,
   })
 }
