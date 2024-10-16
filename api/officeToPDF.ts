24867d264abe5714cf047b0db1d8ec3f78fb0f9a
jammy312 <james.brutus@polymtl.ca>
2024-10-15 22:03:05 -0400

    replace order

--- api/officeToPDF.ts
+++ api/officeToPDF.ts
@@ -32,7 +32,7 @@ const EXTENSION = ".docx"
 const filePath = path.join(FILE_PATH, FILE_NAME + EXTENSION)
 const filePDFPath = path.join(FILE_PATH, FILE_NAME + ".pdf")
 
-const convertFunction: (()=>Promise <void>)[] = [useAdobeDeveloper, useCloudConvert, useConvertAPI];
+const convertFunction: (()=>Promise <void>)[] = [ useCloudConvert, useConvertAPI,useAdobeDeveloper];
 
 export async function POST(req: NextRequest) {
   const body:OfficeToPDFProps = await req.json();
