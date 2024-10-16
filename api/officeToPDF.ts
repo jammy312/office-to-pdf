d78f3e86e12efeae5c427f8a00cf268ebc023656
jammy312 <james.brutus@polymtl.ca>
2024-10-15 21:53:32 -0400

    add adobeDevelopper

--- api/officeToPDF.ts
+++ api/officeToPDF.ts
@@ -10,6 +10,16 @@ import ConvertAPI from 'convertapi';
 import { NextRequest } from 'next/server'
 import axios from 'axios';
 
+import {  ServicePrincipalCredentials,
+  PDFServices,
+  MimeType,
+  CreatePDFJob,
+  CreatePDFParams,
+  CreatePDFResult,
+  DocumentLanguage,
+  SDKError,
+  ServiceUsageError,
+  ServiceApiError} from "@adobe/pdfservices-node-sdk"
 
 interface OfficeToPDFProps{
   buffer:any
@@ -22,7 +32,7 @@ const EXTENSION = ".docx"
 const filePath = path.join(FILE_PATH, FILE_NAME + EXTENSION)
 const filePDFPath = path.join(FILE_PATH, FILE_NAME + ".pdf")
 
-const convertFunction: (()=>Promise <void>)[] = [useCloudConvert, useConvertAPI];
+const convertFunction: (()=>Promise <void>)[] = [useAdobeDeveloper, useCloudConvert, useConvertAPI];
 
 export async function POST(req: NextRequest) {
   const body:OfficeToPDFProps = await req.json();
@@ -141,3 +151,59 @@ async function useConvertAPI() {
 }
 
 
+
+
+async function useAdobeDeveloper() {
+  let readStream;
+  if(!process.env.PDF_SERVICES_CLIENT_ID || !process.env.PDF_SERVICES_CLIENT_SECRET)
+    return;
+  try {
+  
+      // Initial setup, create credentials instance
+      const credentials = new ServicePrincipalCredentials({
+          clientId:process.env.PDF_SERVICES_CLIENT_ID,
+          clientSecret: process.env.PDF_SERVICES_CLIENT_SECRET
+      });
+
+      // Creates a PDF Services instance
+      const pdfServices = new PDFServices({credentials});
+
+      // Creates an asset(s) from source file(s) and upload
+      readStream = fs.createReadStream(filePath);
+      const inputAsset = await pdfServices.upload({
+          readStream,
+          mimeType: MimeType.DOCX
+      });
+
+      // Creates a new job instance
+      const job = new CreatePDFJob({inputAsset});
+
+      // Submit the job and get the job result
+      const pollingURL = await pdfServices.submit({job});
+      const pdfServicesResponse = await pdfServices.getJobResult({
+          pollingURL,
+          resultType: CreatePDFResult
+      });
+
+      // Get content from the resulting asset(s)
+      const resultAsset = pdfServicesResponse.result?.asset;
+      if(resultAsset){
+        const streamAsset = await pdfServices.getContent({asset: resultAsset});
+
+        // Creates an output stream and copy result asset's content to it
+        const outputFilePath = filePDFPath;
+        console.log(`Saving asset at ${outputFilePath}`);
+
+        const outputStream = fs.createWriteStream(outputFilePath);
+        streamAsset.readStream.pipe(outputStream);
+      }
+  } catch (err) {
+      if (err instanceof SDKError || err instanceof ServiceUsageError || err instanceof ServiceApiError) {
+          console.log("Exception encountered while executing operation", err);
+      } else {
+          console.log("Exception encountered while executing operation", err);
+      }
+  } finally {
+      readStream?.destroy();
+  }
+}
